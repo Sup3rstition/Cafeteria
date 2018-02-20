2e0f65131c6a440bc6764b7c2c2a0ec3a120db5f
Bryan <Cordes96@gmail.com>
2018-02-20 12:17:48 -0500

    Gui more updated
    Bryan updated more of the gui.

--- Cafeteria/src/OrderPage.java
+++ Cafeteria/src/OrderPage.java
@@ -36,6 +36,13 @@ import javax.swing.JScrollBar;
 import javax.swing.JSpinner;
 import javax.swing.SpringLayout;
 import java.awt.Font;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class OrderPage extends JFrame {
 
@@ -45,7 +52,7 @@ public class OrderPage extends JFrame {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private String[] column_headers = {"Extras","#"};
 	private String[][] extras = {
-	{"Chips","0"},{"Soda","0"},{"Extra","3"},{"Extra","4"},{"Extra","5"},{"Extra","6"},{"Extra","7"},{"Extra","8"},{"Extra","9"},{"Extra","10"},
+	{"Extra","1"},{"Extra","2"},{"Extra","3"},{"Extra","4"},{"Extra","5"},{"Extra","6"},{"Extra","7"},{"Extra","8"},{"Extra","9"},{"Extra","10"},
 	{"Extra","11"},{"Extra","12"},{"Extra","13"},{"Extra","14"},{"Extra","15"},{"Extra","16"},{"Extra","17"},{"Extra","18"},{"Extra","19"},{"Extra","20"},
 	{"Extra","21"},{"Extra","22"},{"Extra","23"},{"Extra","24"},{"Extra","25"},{"Extra","26"},{"Extra","27"},{"Extra","28"},{"Extra","29"},{"Extra","30"},
 	{"Extra","31"},{"Extra","32"},{"Extra","33"},{"Extra","34"},{"Extra","35"},{"Extra","36"},{"Extra","37"},{"Extra","38"},{"Extra","39"},{"Extra","40"}};
@@ -73,7 +80,7 @@ public class OrderPage extends JFrame {
 	 */
 	public OrderPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 626,487);
+		setBounds(100, 100, 621,487);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -87,13 +94,19 @@ public class OrderPage extends JFrame {
 		
 		JLabel lblCurrentDate = new JLabel("Current Date:");
 		
-		JLabel label_3 = new JLabel("1/1/2000");
+		JLabel label_3 = new JLabel("??/??/????");
 		
-		JLabel label_4 = new JLabel("AccountName");
+		JLabel lblparentname = new JLabel("(ParentName)");
 		
-		JLabel label_5 = new JLabel("$0.00");
+		JLabel label_5 = new JLabel("??.??");
 		
 		JButton button = new JButton("Check History");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
 		
 		JButton button_1 = new JButton("Log Out");
 		GroupLayout gl_panel = new GroupLayout(panel);
@@ -116,7 +129,7 @@ public class OrderPage extends JFrame {
 								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 									.addComponent(label_5)
 									.addComponent(label_3))
-								.addComponent(label_4))))
+								.addComponent(lblparentname))))
 					.addContainerGap(25, Short.MAX_VALUE))
 		);
 		gl_panel.setVerticalGroup(
@@ -124,7 +137,7 @@ public class OrderPage extends JFrame {
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(label_4)
+						.addComponent(lblparentname)
 						.addComponent(label))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
@@ -160,11 +173,13 @@ public class OrderPage extends JFrame {
 		textField_1.setColumns(10);
 		
 		JButton button_2 = new JButton("Order");
+		
+		JButton btnClear = new JButton("Clear");
 		GroupLayout gl_layeredPane = new GroupLayout(layeredPane);
 		gl_layeredPane.setHorizontalGroup(
 			gl_layeredPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_layeredPane.createSequentialGroup()
-					.addContainerGap(15, Short.MAX_VALUE)
+					.addContainerGap()
 					.addGroup(gl_layeredPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_layeredPane.createSequentialGroup()
 							.addGroup(gl_layeredPane.createParallelGroup(Alignment.TRAILING)
@@ -178,6 +193,8 @@ public class OrderPage extends JFrame {
 									.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)))
 							.addGap(5))
 						.addGroup(gl_layeredPane.createSequentialGroup()
+							.addComponent(btnClear)
+							.addPreferredGap(ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
 							.addComponent(button_2)
 							.addContainerGap())))
 		);
@@ -193,12 +210,14 @@ public class OrderPage extends JFrame {
 						.addComponent(label_15)
 						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(button_2)
+					.addGroup(gl_layeredPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(button_2)
+						.addComponent(btnClear))
 					.addGap(37))
 		);
 		layeredPane.setLayout(gl_layeredPane);
 		
-		JLabel lblProblemsContactUs = new JLabel("Problems? Contact us at _");
+		JLabel lblProblemsContactUs = new JLabel("Problems? Contact the school.");
 		
 		JList list = new JList();
 		list.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -272,43 +291,48 @@ public class OrderPage extends JFrame {
 		panel_3.setLayout(sl_panel_3);
 		
 		JLabel lblMenu_1 = new JLabel("Menu 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_1, 35, SpringLayout.NORTH, panel_3);
 		panel_3.add(lblMenu_1);
 		
-		JLabel label_16 = new JLabel("Menu 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, label_16, 57, SpringLayout.NORTH, panel_3);
-		sl_panel_3.putConstraint(SpringLayout.WEST, label_16, 10, SpringLayout.WEST, panel_3);
-		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_1, 0, SpringLayout.WEST, label_16);
-		sl_panel_3.putConstraint(SpringLayout.SOUTH, lblMenu_1, -6, SpringLayout.NORTH, label_16);
-		panel_3.add(label_16);
+		JLabel lblMenu_3 = new JLabel("Menu 2:");
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_1, 0, SpringLayout.WEST, lblMenu_3);
+		sl_panel_3.putConstraint(SpringLayout.SOUTH, lblMenu_1, -6, SpringLayout.NORTH, lblMenu_3);
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_3, 73, SpringLayout.NORTH, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_3, 10, SpringLayout.WEST, panel_3);
+		panel_3.add(lblMenu_3);
 		
-		JLabel label_20 = new JLabel("Menu 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, label_20, 6, SpringLayout.SOUTH, label_16);
-		sl_panel_3.putConstraint(SpringLayout.WEST, label_20, 10, SpringLayout.WEST, panel_3);
-		panel_3.add(label_20);
+		JLabel lblMenu_4 = new JLabel("Menu 3:");
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_4, 10, SpringLayout.WEST, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_4, 6, SpringLayout.SOUTH, lblMenu_3);
+		panel_3.add(lblMenu_4);
 		
 		JLabel lblAdd = new JLabel("Add 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, lblAdd, 6, SpringLayout.SOUTH, label_20);
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblAdd, 6, SpringLayout.SOUTH, lblMenu_4);
 		sl_panel_3.putConstraint(SpringLayout.WEST, lblAdd, 10, SpringLayout.WEST, panel_3);
 		panel_3.add(lblAdd);
 		
-		JLabel label_21 = new JLabel("Add 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, label_21, 6, SpringLayout.SOUTH, lblAdd);
-		sl_panel_3.putConstraint(SpringLayout.WEST, label_21, 10, SpringLayout.WEST, panel_3);
-		panel_3.add(label_21);
+		JLabel lblAdd_1 = new JLabel("Add 2:");
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblAdd_1, 6, SpringLayout.SOUTH, lblAdd);
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblAdd_1, 10, SpringLayout.WEST, panel_3);
+		panel_3.add(lblAdd_1);
 		
-		JLabel label_22 = new JLabel("Add 1:");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, label_22, 6, SpringLayout.SOUTH, label_21);
-		sl_panel_3.putConstraint(SpringLayout.WEST, label_22, 10, SpringLayout.WEST, panel_3);
-		panel_3.add(label_22);
+		JLabel lblAdd_2 = new JLabel("Add 3:");
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblAdd_2, 6, SpringLayout.SOUTH, lblAdd_1);
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblAdd_2, 10, SpringLayout.WEST, panel_3);
+		panel_3.add(lblAdd_2);
 		
 		JLabel lblMenu_2 = new JLabel("Menu");
-		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_2, 0, SpringLayout.NORTH, panel_3);
-		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_2, 39, SpringLayout.WEST, panel_3);
-		sl_panel_3.putConstraint(SpringLayout.SOUTH, lblMenu_2, -6, SpringLayout.NORTH, lblMenu_1);
-		sl_panel_3.putConstraint(SpringLayout.EAST, lblMenu_2, 89, SpringLayout.WEST, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_1, 25, SpringLayout.SOUTH, lblMenu_2);
+		sl_panel_3.putConstraint(SpringLayout.NORTH, lblMenu_2, 10, SpringLayout.NORTH, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.WEST, lblMenu_2, 43, SpringLayout.WEST, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.SOUTH, lblMenu_2, -198, SpringLayout.SOUTH, panel_3);
+		sl_panel_3.putConstraint(SpringLayout.EAST, lblMenu_2, 93, SpringLayout.WEST, panel_3);
 		lblMenu_2.setFont(new Font("Lucida Grande", Font.BOLD, 17));
 		panel_3.add(lblMenu_2);
+		
+		JLabel lblWeek_1 = new JLabel("??/??/????");
+		sl_panel_3.putConstraint(SpringLayout.SOUTH, lblWeek_1, -6, SpringLayout.NORTH, lblMenu_1);
+		sl_panel_3.putConstraint(SpringLayout.EAST, lblWeek_1, 0, SpringLayout.EAST, lblMenu_2);
+		panel_3.add(lblWeek_1);
 		
 		JLabel lblExtras = new JLabel("Extras");
 		DefaultTableModel model = new DefaultTableModel(extras,column_headers);
@@ -338,9 +362,10 @@ public class OrderPage extends JFrame {
 		);
 		panel_2.setLayout(gl_panel_2);
 		
-		JLabel label_17 = new JLabel("Order Date:");
+		JLabel lblDay = new JLabel("Day:");
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"}));
 		
 		JLabel lblMenu = new JLabel("Menu");
 		
@@ -387,22 +412,28 @@ public class OrderPage extends JFrame {
 		
 		JSpinner spinner_2 = new JSpinner();
 		SpringLayout sl_panel_1 = new SpringLayout();
-		sl_panel_1.putConstraint(SpringLayout.NORTH, comboBox, -4, SpringLayout.NORTH, label_17);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, spinner_2, -1, SpringLayout.NORTH, rdbtnMenu_2);
+		sl_panel_1.putConstraint(SpringLayout.EAST, spinner_2, 0, SpringLayout.EAST, spinner_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, spinner, 171, SpringLayout.WEST, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, spinner_1, 171, SpringLayout.WEST, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.SOUTH, spinner, -3, SpringLayout.NORTH, spinner_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, lblExtra, 31, SpringLayout.EAST, rdbtnMenu_1);
+		sl_panel_1.putConstraint(SpringLayout.EAST, lblExtra, -6, SpringLayout.WEST, spinner_1);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, spinner_1, -1, SpringLayout.NORTH, rdbtnMenu_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, lblExtra_1, 31, SpringLayout.EAST, rdbtnMenu_2);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblExtra, 4, SpringLayout.NORTH, rdbtnMenu_1);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblExtra_1, 4, SpringLayout.NORTH, rdbtnMenu_2);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, comboBox, -4, SpringLayout.NORTH, lblDay);
 		sl_panel_1.putConstraint(SpringLayout.WEST, comboBox, 91, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.EAST, comboBox, 0, SpringLayout.EAST, lblAdditionalItems);
-		sl_panel_1.putConstraint(SpringLayout.WEST, label_17, 0, SpringLayout.WEST, lblMenu);
-		sl_panel_1.putConstraint(SpringLayout.EAST, label_17, 72, SpringLayout.WEST, lblMenu);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, lblExtra_1, 4, SpringLayout.NORTH, rdbtnMenu_2);
-		sl_panel_1.putConstraint(SpringLayout.WEST, lblExtra_1, 0, SpringLayout.WEST, lblExtra);
+		sl_panel_1.putConstraint(SpringLayout.WEST, lblDay, 0, SpringLayout.WEST, lblMenu);
+		sl_panel_1.putConstraint(SpringLayout.EAST, lblDay, 72, SpringLayout.WEST, lblMenu);
 		sl_panel_1.putConstraint(SpringLayout.WEST, rdbtnMenu_1, 8, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.WEST, rdbtnMenu_2, 8, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.WEST, rdbtnMenu, 8, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.EAST, lblExta, -13, SpringLayout.WEST, spinner);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, lblExtra, 17, SpringLayout.SOUTH, lblExta);
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblExta, 4, SpringLayout.NORTH, rdbtnMenu);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, rdbtnMenu_1, -6, SpringLayout.NORTH, rdbtnMenu_2);
-		sl_panel_1.putConstraint(SpringLayout.WEST, lblExtra, 117, SpringLayout.WEST, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.EAST, lblExtra, -6, SpringLayout.WEST, spinner_1);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, rdbtnMenu_2, -10, SpringLayout.SOUTH, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.NORTH, rdbtnMenu, 6, SpringLayout.SOUTH, lblMenu);
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblAdditionalItems, 103, SpringLayout.WEST, panel_1);
@@ -411,12 +442,6 @@ public class OrderPage extends JFrame {
 		sl_panel_1.putConstraint(SpringLayout.EAST, lblAdditionalItems, -36, SpringLayout.EAST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblMenu, 0, SpringLayout.NORTH, lblAdditionalItems);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, lblAdditionalItems, -6, SpringLayout.NORTH, spinner);
-		sl_panel_1.putConstraint(SpringLayout.WEST, spinner, 0, SpringLayout.WEST, spinner_2);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, spinner, -6, SpringLayout.NORTH, spinner_1);
-		sl_panel_1.putConstraint(SpringLayout.EAST, spinner_2, 0, SpringLayout.EAST, spinner_1);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, spinner_2, -3, SpringLayout.SOUTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.SOUTH, spinner_1, -34, SpringLayout.SOUTH, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.WEST, spinner_1, 171, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.NORTH, label_19, 264, SpringLayout.NORTH, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.WEST, label_19, 393, SpringLayout.WEST, panel_1);
 		sl_panel_1.putConstraint(SpringLayout.SOUTH, label_19, 285, SpringLayout.NORTH, panel_1);
@@ -468,7 +493,7 @@ public class OrderPage extends JFrame {
 		panel_1.add(label_13);
 		panel_1.add(label_18);
 		panel_1.add(label_19);
-		panel_1.add(label_17);
+		panel_1.add(lblDay);
 		panel_1.add(comboBox);
 		panel_1.add(lblMenu);
 		panel_1.add(rdbtnMenu);
@@ -484,24 +509,41 @@ public class OrderPage extends JFrame {
 		
 		JLabel lblWeek = new JLabel("Week:");
 		sl_panel_1.putConstraint(SpringLayout.WEST, lblWeek, 8, SpringLayout.WEST, panel_1);
-		sl_panel_1.putConstraint(SpringLayout.NORTH, label_17, 6, SpringLayout.SOUTH, lblWeek);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblDay, 6, SpringLayout.SOUTH, lblWeek);
 		sl_panel_1.putConstraint(SpringLayout.NORTH, lblWeek, 10, SpringLayout.NORTH, panel_1);
 		panel_1.add(lblWeek);
 		
 		JLabel label_6 = new JLabel("??/??/??-??/??/??");
 		sl_panel_1.putConstraint(SpringLayout.NORTH, label_6, 0, SpringLayout.NORTH, lblWeek);
-		sl_panel_1.putConstraint(SpringLayout.EAST, label_6, 0, SpringLayout.EAST, spinner_2);
+		sl_panel_1.putConstraint(SpringLayout.EAST, label_6, -60, SpringLayout.EAST, panel_1);
 		panel_1.add(label_6);
 		
 		JLabel label_23 = new JLabel("Student:");
-		sl_panel_1.putConstraint(SpringLayout.NORTH, label_23, 8, SpringLayout.SOUTH, label_17);
-		sl_panel_1.putConstraint(SpringLayout.WEST, label_23, 0, SpringLayout.WEST, label_17);
+		sl_panel_1.putConstraint(SpringLayout.NORTH, label_23, 8, SpringLayout.SOUTH, lblDay);
+		sl_panel_1.putConstraint(SpringLayout.WEST, label_23, 0, SpringLayout.WEST, lblDay);
 		panel_1.add(label_23);
 		
 		JLabel label_24 = new JLabel("First Last");
 		sl_panel_1.putConstraint(SpringLayout.NORTH, label_24, 0, SpringLayout.NORTH, label_23);
-		sl_panel_1.putConstraint(SpringLayout.WEST, label_24, 0, SpringLayout.WEST, lblExtra_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, label_24, 57, SpringLayout.EAST, label_23);
 		panel_1.add(label_24);
 		contentPane.setLayout(gl_contentPane);
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
 	}
 }
