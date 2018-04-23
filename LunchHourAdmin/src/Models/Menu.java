5a3aef3bdf6c082c60d34faeb853911cca153986
Bryan <Cordes96@gmail.com>
2018-04-22 21:03:04 -0400

    Updating menueditor
    Adding multiple week menu creator and editor.

--- LunchHourAdmin/src/Models/Menu.java
+++ LunchHourAdmin/src/Models/Menu.java
@@ -1,13 +1,12 @@
 package Models;
 
+import java.sql.Date;
+
+import javafx.beans.property.SimpleStringProperty;
+
 public class Menu {
 private java.sql.Date start;
-public java.sql.Date getStart() {
-	return start;
-}
-public void setStart(java.sql.Date start) {
-	this.start = start;
-}
+private java.sql.Date end;
 private String menu1;
 private String menu2;
 private String menu3;
@@ -20,7 +19,125 @@ private String menu3price;
 private String add1price;
 private String add2price;
 private String add3price;
+private String menu1des;
+private String menu2des;
+private String menu3des;
+private String add1des;
+private String add2des;
+private String add3des;
 
+public SimpleStringProperty getstart() {
+	return new SimpleStringProperty(start.toString());
+}
+public SimpleStringProperty getend() {
+	return new SimpleStringProperty(end.toString());
+}
+public SimpleStringProperty getmenu1() {
+	return new SimpleStringProperty(menu1);
+}
+public SimpleStringProperty getmenu2() {
+	return new SimpleStringProperty(menu2);
+}
+public SimpleStringProperty getmenu3() {
+	return new SimpleStringProperty(menu3);
+}
+public SimpleStringProperty getadd1() {
+	return new SimpleStringProperty(add1);
+}
+public SimpleStringProperty getadd2() {
+	return new SimpleStringProperty(add2);
+}
+public SimpleStringProperty getadd3() {
+	return new SimpleStringProperty(add3);
+}
+public SimpleStringProperty getadd1price() {
+	return new SimpleStringProperty(add1price);
+}
+public SimpleStringProperty getadd2price() {
+	return new SimpleStringProperty(add2price);
+}
+public SimpleStringProperty getadd3price() {
+	return new SimpleStringProperty(add3price);
+}
+public SimpleStringProperty getmenu1price() {
+	return new SimpleStringProperty(menu1price);
+}
+public SimpleStringProperty getmenu2price() {
+	return new SimpleStringProperty(menu2price);
+}
+public SimpleStringProperty getmenu3price() {
+	return new SimpleStringProperty(menu3price);
+}
+public SimpleStringProperty getmenu1des() {
+	return new SimpleStringProperty(menu1des);
+}
+public SimpleStringProperty getmenu2des() {
+	return new SimpleStringProperty(menu2des);
+}
+public SimpleStringProperty getmenu3des() {
+	return new SimpleStringProperty(menu3des);
+}
+public SimpleStringProperty getadd1des() {
+	return new SimpleStringProperty(add1des);
+}
+public SimpleStringProperty getadd2des() {
+	return new SimpleStringProperty(add2des);
+}
+public SimpleStringProperty getadd3des() {
+	return new SimpleStringProperty(add3des);
+}
+public java.sql.Date getEnd() {
+	return end;
+}
+public java.sql.Date getStart() {
+	return start;
+}
+public void setStart(java.sql.Date start) {
+	this.start = start;
+}
+private int Menuid;
+public int getMenuid() {
+	return Menuid;
+}
+public void setMenuid(int menuid) {
+	Menuid = menuid;
+}
+public String getMenu1des() {
+	return menu1des;
+}
+public void setMenu1des(String menu1des) {
+	this.menu1des = menu1des;
+}
+public String getMenu2des() {
+	return menu2des;
+}
+public void setMenu2des(String menu2des) {
+	this.menu2des = menu2des;
+}
+public String getMenu3des() {
+	return menu3des;
+}
+public void setMenu3des(String menu3des) {
+	this.menu3des = menu3des;
+}
+public String getAdd1des() {
+	return add1des;
+}
+public void setAdd1des(String add1des) {
+	this.add1des = add1des;
+}
+public String getAdd2des() {
+	return add2des;
+}
+public void setAdd2des(String add2des) {
+	this.add2des = add2des;
+}
+public String getAdd3des() {
+	return add3des;
+}
+public void setAdd3des(String add3des) {
+	this.add3des = add3des;
+}
 public String getMenu1() {
 	return menu1;
 }
@@ -93,15 +210,8 @@ public String getAdd3price() {
 public void setAdd3price(String add3price) {
 	this.add3price = add3price;
 }
-class Menu1{
-	private String menu1item;
-
-	public String getMenu1item() {
-		return menu1item;
-	}
-
-	public void setMenu1item(String menu1item) {
-		this.menu1item = menu1item;
-	}
+public void setEnd(Date date) {
+	this.end = date;
+	
 }
 }
--- LunchHourAdmin/src/controllers/Menucontroller.java
+++ LunchHourAdmin/src/controllers/Menucontroller.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ResourceBundle;
 
 import Models.Menu;
@@ -21,10 +29,14 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Tooltip;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class Menucontroller implements Initializable {
 	private Connection conn = null;
@@ -91,7 +103,8 @@ public class Menucontroller implements Initializable {
     private Label add3;
     @FXML
     private Label m1price;
-
+    @FXML
+    private Label enddate;
     @FXML
     private Label m2price;
 
@@ -103,22 +116,92 @@ public class Menucontroller implements Initializable {
 
     @FXML
     private Label add2price;
-
+    @FXML
+    private DatePicker startingdate;
     @FXML
     private Label add3price;
+	@FXML
+    private Tooltip menu1des;
+	@FXML
+    private Tooltip menu2des;
+	 @FXML
+	    private Tooltip menu3des;
+	 @FXML
+	    private Tooltip add1des;
+	 @FXML
+	    private Tooltip add2des;
+	 @FXML
+	    private Tooltip add3des;
     void setitems() {
-		menu1.setText(menu.getMenu1());
-		m1price.setText(menu.getMenu1price());
-		menu2.setText(menu.getMenu2());
-		m2price.setText(menu.getMenu2price());
-		menu3.setText(menu.getMenu3());
-		m3price.setText(menu.getMenu3price());
-		add1.setText(menu.getAdd1());
-		add2.setText(menu.getAdd2());
-		add3.setText(menu.getAdd3());
-		add1price.setText(menu.getAdd1price());
-		add2price.setText(menu.getAdd2price());
-		add3price.setText(menu.getAdd3price());
+    	if(!menu.getMenu1().equals("No Item")) {
+    		menu1.setDisable(false);
+    		menu1.setText(menu.getMenu1());
+    		m1price.setText(menu.getMenu1price());
+    		menu1des.setText(menu.getMenu1des());
+    	}else {
+    		menu1.setText("No Item");
+    		m1price.setText("0");
+    		menu1.setDisable(true);
+    	}
+    	
+    	if(!menu.getMenu2().equals("No Item")) {
+    		menu2.setDisable(false);
+    		menu2.setText(menu.getMenu2());
+    		m2price.setText(menu.getMenu2price());
+    		menu2des.setText(menu.getMenu2des());
+    		}else {
+    		menu2.setText("No Item");
+    		m2price.setText("0");
+    		menu2.setDisable(true);
+    	}
+    	
+    	if(!menu.getMenu3().equals("No Item")) {
+    		menu3.setDisable(false);
+    		menu3.setText(menu.getMenu3());
+    		m3price.setText(menu.getMenu3price());
+    		menu3des.setText(menu.getMenu3des());
+    		
+    	}else {
+    		menu3.setText("No Item");
+    		m3price.setText("0");
+    		menu3.setDisable(true);
+    	}
+    	
+    	if(!menu.getAdd1().equals("No Item")) {
+    		add1.setDisable(false);
+    		add1.setText(menu.getAdd1());
+    		add1price.setText(menu.getAdd1price());
+    		add1des.setText(menu.getAdd1des());
+    		
+    		
+    	}else {
+    		add1.setText("No Item");
+    		add1price.setText("0");
+    		add1.setDisable(true);
+    	}
+    	
+    	if(!menu.getAdd2().equals("No Item")) {
+    		add2.setDisable(false);
+    		add2.setText(menu.getAdd2());
+    		add2price.setText(menu.getAdd2price());
+    		add2des.setText(menu.getAdd2des());
+    		
+    	}else {
+    		add2.setText("No Item");
+    		add2price.setText("0");
+    		add2.setDisable(true);
+    	}
+    	
+    	if(!menu.getAdd3().equals("No Item")) {
+    		add3.setDisable(false);
+    		add3.setText(menu.getAdd3());
+    		add3price.setText(menu.getAdd3price());
+    		add3des.setText(menu.getAdd3des());
+    	}else {
+    		add3.setText("No Item");
+    		add3price.setText("0");
+    		add3.setDisable(true);
+    	}
 	}
 
     @FXML
@@ -290,49 +373,125 @@ public class Menucontroller implements Initializable {
 	public Menu getMenu() {
 			return menu;
 		}
-
+	DateTimeFormatter dateformatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 		public void setMenu(Menu menu) {
 			this.menu = menu;
 		}
+		private java.sql.Date dateMenu(LocalDate date) {
+			if(date.compareTo(LocalDate.now().with(DayOfWeek.MONDAY)) == 0) {
+			java.sql.Date first = java.sql.Date.valueOf(LocalDate.now().with(DayOfWeek.MONDAY));
+			 LocalDate now = LocalDate.now();
+			 DayOfWeek day = DayOfWeek.from(now);
+		     LocalDateTime date1 = LocalDateTime.now();
+		     LocalDateTime date2 = now.atStartOfDay().plusHours(7);
+			 if(day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY || (date1.isAfter(date2)&& day == DayOfWeek.FRIDAY) ) {
+				  first = java.sql.Date.valueOf(now.with(TemporalAdjusters.next(DayOfWeek.MONDAY)));
+			 }
+			 startingdate.setValue(first.toLocalDate());
+			 enddate.setText(dateformatter.format(first.toLocalDate().with(DayOfWeek.FRIDAY)));
+			 return first;
+			}else {
+				java.sql.Date first = java.sql.Date.valueOf(startingdate.getValue());
+				return first;
+			}
+		}
 			@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		if(menu == null) {
-		
-		String SQL = "SELECT * from Menu";
-			try {
-				conn = Lunchhourdb.get();
-				ps = conn.prepareStatement(SQL);
-				rs = ps.executeQuery();
-				if(rs.next()) {
-					Menu menu = new Menu();
-					menu.setMenu1(rs.getString("Menu 1 item"));
-					menu.setMenu2(rs.getString("Menu 2 item"));
-					menu.setMenu3(rs.getString("Menu 3 item"));
-					menu.setMenu1price(rs.getString("Menu 1 Price"));
-					menu.setMenu2price(rs.getString("Menu 2 Price"));
-					menu.setMenu3price(rs.getString("Menu 3 Price"));
-					menu.setStart(rs.getDate("Menu_Start"));
-					menu.setAdd1(rs.getString("Additional 1"));
-					menu.setAdd2(rs.getString("Additional 2"));
-					menu.setAdd3(rs.getString("Additional 3"));
-					menu.setAdd1price(rs.getString("Add 1 Price"));
-					menu.setAdd2price(rs.getString("Add 2 Price"));
-					menu.setAdd3price(rs.getString("Add 3 Price"));
-					this.menu = menu;
+	}
+			void firststart() {
+					 orderdate();
+					 LocalDate now = LocalDate.now();
+					 DayOfWeek day = DayOfWeek.from(now);
+				     LocalDateTime date1 = LocalDateTime.now();
+				     LocalDateTime date2 = now.atStartOfDay().plusHours(7);
+					 if( day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY || (day == DayOfWeek.FRIDAY  && date1.isAfter(date2) )) {
+						 startingdate.setValue(now.with(TemporalAdjusters.next(DayOfWeek.MONDAY)));
+					 }else {
+						 startingdate.setValue(now.with(DayOfWeek.MONDAY));
+					 
+					 }
+					createMenu(dateMenu(startingdate.getValue()));
 					setitems();
 				}
-			} catch (SQLException e) {
-				Alert Error2= new Alert(AlertType.ERROR, "An error has occured while connecting with the database.\n Please check your internet connection and try again.");
-    	   		Error2.setTitle("Error");
-    	   		Error2.setHeaderText("Connection Error!");
-    	   		Error2.showAndWait();
-			}
-		}else {
-			setitems();
-		}
-				
-		
-	}
+			
 
-	
+			void createMenu(java.sql.Date first) {
+
+				String SQL = "SELECT * from Menu INNER JOIN Menutool ON Menu.menuid=Menutool.menuidtool Where Menu_Start = ?";
+				try {
+					conn = Lunchhourdb.get();
+					ps = conn.prepareStatement(SQL);
+					System.out.println(first);
+					ps.setDate(1, first);
+					rs = ps.executeQuery();
+					if(rs.next()) {
+						menu = new Menu();
+						menu.setMenuid(rs.getInt("Revision"));
+						menu.setMenu1(rs.getString("Menu 1 item"));
+						menu.setMenu2(rs.getString("Menu 2 item"));
+						menu.setMenu3(rs.getString("Menu 3 item"));
+						menu.setMenu1price(rs.getString("Menu 1 Price"));
+						menu.setMenu2price(rs.getString("Menu 2 Price"));
+						menu.setMenu3price(rs.getString("Menu 3 Price"));
+						menu.setStart(rs.getDate("Menu_Start"));
+						menu.setEnd(rs.getDate("Menu_End"));
+						menu.setAdd1(rs.getString("Additional 1"));
+						menu.setAdd2(rs.getString("Additional 2"));
+						menu.setAdd3(rs.getString("Additional 3"));
+						menu.setAdd1price(rs.getString("Add 1 Price"));
+						menu.setAdd2price(rs.getString("Add 2 Price"));
+						menu.setAdd3price(rs.getString("Add 3 Price"));
+						menu.setMenu1des(rs.getString("Menu1des"));
+						menu.setMenu2des(rs.getString("Menu2des"));
+						menu.setMenu3des(rs.getString("Menu3des"));
+						menu.setAdd1des(rs.getString("add1des"));
+						menu.setAdd2des(rs.getString("add2des"));
+						menu.setAdd3des(rs.getString("add3des"));
+						
+					}
+				} catch (SQLException e) {
+					Alert Error2= new Alert(AlertType.ERROR, "An error has occured while connecting with the database.\n Please check your internet connection and try again.");
+			   		Error2.setTitle("Error");
+			   		Error2.setHeaderText("Connection Error!");
+			   		Error2.showAndWait();
+				}finally {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			private void orderdate() {
+		        final Callback<DatePicker, DateCell> dayCellFactory = 
+		            new Callback<DatePicker, DateCell>() {
+		                @Override
+		                public DateCell call(final DatePicker datePicker) {
+		                    return new DateCell() {
+		                        @Override
+		                        public void updateItem(LocalDate item, boolean empty) {
+		                            super.updateItem(item, empty);
+		                            LocalDate now = LocalDate.now();
+		                            DayOfWeek day = DayOfWeek.from(item);
+		                            LocalDateTime date1 = LocalDateTime.now();
+		                            LocalDateTime date2 = now.atStartOfDay().plusHours(7);
+		                            if (day != DayOfWeek.MONDAY || item.isBefore(now.with(DayOfWeek.MONDAY)) || (day == DayOfWeek.FRIDAY  && date1.isAfter(date2) ) ){
+		                                    setDisable(true);
+		                                    setStyle("-fx-background-color: #ffc0cb;");
+		                            }
+
+		                    }
+		                };
+		            }
+			};
+			startingdate.setDayCellFactory(dayCellFactory);
+			 
+			 startingdate.valueProperty().addListener((ov, oldValue, newValue) -> {
+				 if(newValue != oldValue) {
+						createMenu(dateMenu(newValue));
+						enddate.setText(dateformatter.format((newValue.plusDays(4))));
+				 }
+		        });
+			}
 }
